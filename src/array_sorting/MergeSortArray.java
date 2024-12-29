a73458706c120ebd2856ace261134d519fe589f2
RoMANzhula <roman1985al@ukr.net>
2024-12-29 21:51:07 +0200

    Add array sorting - merge sort array

---
+++ src/array_sorting/MergeSortArray.java
@@ -0,0 +1,52 @@
+package array_sorting;
+
+import java.util.Arrays;
+
+public class MergeSortArray {
+    public static void main(String[] args) {
+        int[] notSortArray = {12, 7, 11, 4, 7, 10, 2};
+
+        mergeSort(notSortArray, 0, notSortArray.length - 1);
+
+        System.out.println(Arrays.toString(notSortArray));
+
+    }
+
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (left < right) {
+            int middle = (left + right) / 2;
+            mergeSort(arr, left, middle);
+            mergeSort(arr, middle + 1, right);
+            merge(arr, left, middle, right);
+        }
+    }
+
+    private static void merge(int[] arr, int left, int middle, int right) {
+        int n1 = middle - left + 1;
+        int n2 = right - middle;
+        int[] leftArr = new int[n1];
+        int[] rightArr = new int[n2];
+
+        System.arraycopy(arr, left, leftArr, 0, n1);
+        System.arraycopy(arr, middle + 1, rightArr, 0, n2);
+
+        int i = 0, j = 0, k = left;
+
+        while (i < n1 && j < n2) {
+            if (leftArr[i] <= rightArr[j]) {
+                arr[k++] = leftArr[i++];
+            } else {
+                arr[k++] = rightArr[j++];
+            }
+        }
+
+        while (i < n1) {
+            arr[k++] = leftArr[i++];
+        }
+
+        while (j < n2) {
+            arr[k++] = rightArr[j++];
+        }
+    }
+
+}
