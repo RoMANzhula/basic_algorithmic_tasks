967068e5bb823cdf3caa1f79a1cd40a92977b7ca
RoMANzhula <roman1985al@ukr.net>
2024-12-29 19:37:03 +0200

    Add factorial of number example

--- src/factorial_of_number/Factorial.java
+++ src/factorial_of_number/Factorial.java
@@ -4,10 +4,23 @@ public class Factorial {
 
     public static void main(String[] args) {
         System.out.println(factorial(4)); // 1 * 2 * 3 * 4 = 24
-        System.out.println(factorial(5)); // 1 * 2 * 3 * 4 * 5 = 120
+
+        // with recursion
+        System.out.println(factorialRecursion(5)); // 1 * 2 * 3 * 4 * 5 = 120
     }
 
-    private static long factorial(int number) {
+    private static long factorial(int num) {
+        long result = 1;
+
+        for (int i = 1; i <= num; i++) {
+            result *= i;
+        }
+
+        return result;
+    }
+
+
+    private static long factorialRecursion(int number) {
         if (number == 0 || number == 1) return 1; // or if (number <= 1)
 
         return number * factorial(number - 1);
